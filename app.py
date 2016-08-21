43af036100008e925f616a78abaa5b26d973e7c6
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-08-20 23:14:14 -0400

    [ADJUST] Novas informações do bot
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from flask import jsonify
 from flask import make_response
 from flask import request
 from flask import url_for
+from flask_cors import CORS, cross_origin
 import os.path
 
 app = Flask(__name__, static_url_path="")
+CORS(app)
 
 k = aiml.Kernel()
 
